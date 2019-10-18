adfe68ddecd1f4d3642a623ab829bd83ff5ff44b
Mikhail Gorbachev <aleksandrow.mike@gmail.com>
2019-10-18 16:45:15 +0300

    city.json

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -128,4 +128,13 @@ sum.addEventListener('click', (event) => {
     });
 });
 
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+    }
+};
+xhttp.open("GET", "city.json", true);
+xhttp.send();
+
 
