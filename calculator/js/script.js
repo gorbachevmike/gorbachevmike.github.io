57f8c4882d4700b7784f608fa8de032aa1d2b9a3
Mikhail Gorbachev <aleksandrow.mike@gmail.com>
2019-10-20 13:50:13 +0300

    Full Update Calculator

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -30,6 +30,45 @@ let elementMounth = document.getElementById('mounth');
 let elementHours = document.getElementById('hours');
 let elementYears = document.getElementById('years');
 let elementMinutes =  document.getElementById('minutes');
+let elementMenuItem = document.getElementById('menuItem');
+
+//Tabs
+
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('content__menu-item-link');
+    hideTabsContent(1);
+}
+
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('active');
+    }
+}
+
+document.getElementById('menuItem').onclick= function (event) {
+    var target=event.target;
+    if (target.className=='content__menu-item-link') {
+       for (var i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+}}}}
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('active');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
+
 
 utc().forEach((value, index) => {
     let option = document.createElement('option');
@@ -107,6 +146,17 @@ function getElementsById(id) {
 }
 
 let result = getElementsById('tableTime');
+let result2 = getElementsById('tableTime2');
+
+
+let resultR = getElementsById('tableTimeR');
+let resultR2 = getElementsById('tableTimeR2');
+
+
+let resultS = getElementsById('tableTimeS');
+let resultS2 = getElementsById('tableTimeS2');
+
+
 var sunDay = 2;
 var theHours = new Date();
 
@@ -116,51 +166,51 @@ tableResult.style.display = "none";
 
 let sum = document.getElementById('sum');
 
-var city = [];
-var cityLL = [];
-var xhttp = new XMLHttpRequest();
+/* Автозаполнение города */
+var token = "5bbca3f4fd3dcaa4f2b4f78a39925ce876332e6a";
 
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var jsonResponse = JSON.parse(this.responseText);
-        for(let i = 0; i < jsonResponse.length; i++){
-            //console.log(jsonResponse[i]);
-            city.push(jsonResponse[i].Город);
-            cityLL.push({"city":jsonResponse[i].Город, "lat": jsonResponse[i].Широта,'lon': jsonResponse[i].Долгота});
-        }
-    }
-};
+var type  = "ADDRESS";
+var $city = $("#city");
 
-new autoComplete({
-    selector: 'input[name="city"]',
-    minChars: 2,
-    source: function(term, suggest){
-        term = term.toLowerCase();
-        var choices = city;
-        var matches = [];
-        for (let i=0; i<choices.length; i++)
-            if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
-        suggest(matches);
-    }
+  
+function iplocate() {
+    var serviceUrl = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address";
+    var params = {
+      type: "GET",
+      contentType: "application/json",
+      headers: {
+        "Authorization": "Token " + token
+      }
+    };
+      return $.ajax(serviceUrl, params);
+}
+  // город и населенный пункт
+  $city.suggestions({
+    token: token,
+    type: type,
+    hint: false,
+    bounds: "city-settlement",
+  });
+  
+  iplocate().done(function(response) {
+      var location = response.location;
+      console.log(location);
+      if (!location) {
+        return;
+      }    
+      $city.suggestions().setSuggestion(location);
 });
+  
+/* /Автозаполнение города */
 
-let elementCity = document.getElementById('city');
 var lat = 55.76;
 var lon = 37.64;
 
-elementCity.addEventListener('change', (event) => {
-    var ch = cityLL;
-    for(let i=0; i<ch.length; i++){
-        if(ch[i].city == event.srcElement.value){
-            lat = ch[i].lat;
-            lon = ch[i].lon;
-        }
-    }
-});
+var city = '';
+
+var elementCity = document.getElementById('city');
 
 
-xhttp.open("GET", "https://aleksandrowmike.github.io/calculator/city.json", true);
-xhttp.send();
 
 ymaps.ready(init);
 
@@ -172,23 +222,11 @@ navigator.geolocation.getCurrentPosition(function(location) {
         center: [location.coords.latitude, location.coords.longitude],
         zoom: 10
     });
-    lat = location.coords.latitude;
-    lon = location.coords.longitude;
-    document.getElementById('lat').value = location.coords.latitude;
-    document.getElementById('lon').value = location.coords.longitude;
-
-    var myPlacemark = new ymaps.GeoObject({
-        geometry: {
-            type: "Point",
-            coordinates: [location.coords.latitude, location.coords.longitude]
-        }
-    });
-
-    myMap.geoObjects.add(myPlacemark);
 
     sum.addEventListener('click', (event) => {
-        tableResult.style.display = "table";
-        titleText.style.visibility = "hidden";
+        tableResult.style.display = "block";
+        titleText.style.display = "none";
+        elementMenuItem.style.display = 'flex';
         result.forEach((value, index) => { 
             if(index%2 == 0){
                 if(index == 0){
@@ -205,18 +243,100 @@ navigator.geolocation.getCurrentPosition(function(location) {
                 value.innerHTML =  theHours.getHours() + ":00";
             }
         });
-    
-        var myPlacemark = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [lat, lon]
+
+        result2.forEach((value, index) => { 
+            if(index%2 == 0){
+                if(index == 0){
+                    let hour = Number(elementUtc.options[elementUtc.selectedIndex].value) + 1;
+                    value.innerHTML = Math.abs(hour) + ":00" ;
+                }else{
+                    let h = (index - 2 + sunDay) - 1 + Number(elementUtc.options[elementUtc.selectedIndex].value) + 2;
+                    theHours.setHours(h);
+                    value.innerHTML = theHours.getHours() + ":00";
+                }
+            }else{
+                let h = (index + sunDay) + 10 + Number(elementUtc.options[elementUtc.selectedIndex].value) + 2;
+                theHours.setHours(h);
+                value.innerHTML =  theHours.getHours() + ":00";
             }
         });
+
+        resultS.forEach((value, index) => { 
+            if(index%2 == 0){
+                if(index == 0){
+                    let hour = Number(elementUtc.options[elementUtc.selectedIndex].value);
+                    value.innerHTML = Math.abs(hour) + ":00" ;
+                }else if(index == 10){
+                    value.innerHTML = "--:--" ;
+                }else{
+                    let h = (index - 2 + sunDay) - 1 + Number(elementUtc.options[elementUtc.selectedIndex].value);
+                    theHours.setHours(h);
+                    value.innerHTML = theHours.getHours() + ":00";
+                }
+            }else{
+                if(index == 11){
+                    value.innerHTML = "--:--" ;
+                }else if(index == 9){
+                    value.innerHTML = "--:--" ;
+                }else {
+                    let h = (index + sunDay) + 10 + Number(elementUtc.options[elementUtc.selectedIndex].value);
+                    theHours.setHours(h);
+                    value.innerHTML =  theHours.getHours() + ":00";
+                }
+                    
+            }
+        });
+
+
+        resultS2.forEach((value, index) => { 
+            if(index%2 == 0){
+                if(index == 0){
+                    let hour = Number(elementUtc.options[elementUtc.selectedIndex].value);
+                    value.innerHTML = Math.abs(hour) + ":41" ;
+                }else if(index == 10){
+                    value.innerHTML = "--:--" ;
+                }else{
+                    let h = (index - 2 + sunDay) - 1 + Number(elementUtc.options[elementUtc.selectedIndex].value)+1;
+                    theHours.setHours(h);
+                    value.innerHTML = theHours.getHours() + ":00";
+                }
+            }else{
+                if(index == 11){
+                    value.innerHTML = "--:--" ;
+                }else if(index == 9){
+                    value.innerHTML = "--:--" ;
+                }else {
+                    let h = (index + sunDay) + 10 + Number(elementUtc.options[elementUtc.selectedIndex].value) +1;
+                    theHours.setHours(h);
+                    value.innerHTML =  theHours.getHours() + ":00";
+                }
+                    
+            }
+        });
+
+        var myGeocoder = ymaps.geocode(elementCity.value);
+        myGeocoder.then(
+            function (res) {
+                var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                var myPlacemark = new ymaps.GeoObject({
+                    geometry: {
+                        type: "Point",
+                        coordinates: res.geoObjects.get(0).geometry.getCoordinates()
+                    }
+                });
+                myMap.geoObjects.add(myPlacemark);
+                myMap.setCenter(myPlacemark.geometry.getCoordinates());
+                document.getElementById('lat').value = coordinates[0];
+                document.getElementById('lon').value = coordinates[1];
+            },
+            function (err) {
+                alert('Ошибка');
+            }
+        );
+        
     
-        document.getElementById('lat').value = lat;
-        document.getElementById('lon').value = lon;
-        myMap.geoObjects.add(myPlacemark);
-        myMap.setCenter(myPlacemark.geometry.getCoordinates());
+       
+        
     });
     
 
